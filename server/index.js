5f401bc9815be3b43dbd7e6c7261f0413e8f526e
David Papamichael <davidpapamichael@gmail.com>
2020-05-27 15:52:05 +0100

    CRUD cycle routes set up

--- server/index.js
+++ server/index.js
@@ -12,8 +12,46 @@ if (process.env.NODE_ENV === "test") {
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", async (req, res) => {
-  res.json({ message: "pass!" });
+app.get("/tweets", async (req, res) => {
+  const allTweets = await pool.query("SELECT * FROM tweets");
+  res.json(allTweets.rows);
+});
+
+app.get("/tweets/:id", async (req, res) => {
+  const { id } = req.params;
+  const tweet = await pool.query("SELECT * FROM tweets WHERE tweet_id = $1", [
+    id,
+  ]);
+
+  res.json(tweet.rows[0]);
+});
+
+app.post("/tweets", async (req, res) => {
+  const { description } = req.body;
+  const newTweet = await pool.query(
+    "INSERT INTO tweets (description) VALUES($1) RETURNING *",
+    [description]
+  );
+  res.json(newTodo.rows[0]);
+});
+
+app.put("/tweets/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  const updateTweet = await pool.query(
+    "UPDATE tweets SET description = $1 WHERE tweet_id = $2",
+    [description, id]
+  );
+  res.json("Tweet was updated!");
+});
+
+app.delete("/tweets/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteTweet = await pool.query(
+    "DELETE FROM tweets WHERE tweet_id = $1",
+    [id]
+  );
+  res.json("Tweet was deleted!");
 });
 
 app.listen(port, () => {
--- server/index.test.js
+++ server/index.test.js
@@ -2,12 +2,17 @@ const app = require("./index");
 const supertest = require("supertest");
 const request = supertest(app);
 
-describe("test", () => {
-  it("gets the test endpoint", async (done) => {
-    const response = await request.get("/tweets");
+beforeAll(() => {
+  process.env.NODE_ENV = "test";
+  await pool.query("INSERT INTO tweets (description) VALUES('this is a test tweet') RETURNING *");
+});
 
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe("pass!");
-    done();
+afterAll(() => {
+  pool.query("TRUNCATE TABLE tweets;");
+});
+
+describe("Test the view all tweets path", () => {
+  test("It should response the GET method", () => {
+    return request.get("/tweets").expect(200);
   });
 });
