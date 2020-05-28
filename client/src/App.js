0569797df1f77436998093406913a3ea7a47f9f8
David Papamichael <davidpapamichael@gmail.com>
2020-05-28 16:36:15 +0100

    enzyme and jest installed, first app test passed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React, { Fragment } from "react";
 import "./App.css";
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="container" />
+    </Fragment>
+  );
 }
 
 export default App;
