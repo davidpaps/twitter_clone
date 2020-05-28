df0ed3d8f7b462ff4d313bccfbe309a0867a761d
David Papamichael <davidpapamichael@gmail.com>
2020-05-28 17:05:02 +0100

    form added with button for sign in, tests passed

--- client/src/components/signIn/signIn.js
+++ client/src/components/signIn/signIn.js
@@ -3,6 +3,12 @@ import React, { Fragment } from "react";
 const SignIn = () => {
   return (
     <Fragment>
+      <h1 className="text-center mt-5">Welcome to Twitter</h1>
+      <form>
+        <input type="email" className="" />
+        <input type="password" className="" />
+        <button>Sign In</button>
+      </form>
       <div className="container" />
     </Fragment>
   );
--- client/src/components/signIn/signIn.test.js
+++ client/src/components/signIn/signIn.test.js
@@ -3,8 +3,15 @@ import { shallow } from "enzyme";
 import SignIn from "./signIn";
 
 describe("Sign In", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<SignIn />)));
+
   it("should render a <div />", () => {
-    const wrapper = shallow(<SignIn />);
     expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should render a <form />", () => {
+    expect(wrapper.find("form").length).toEqual(1);
   });
 });
