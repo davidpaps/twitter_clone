52a65dd14a990682664c012bb92ba62d72387101
David Papamichael <davidpapamichael@gmail.com>
2020-05-29 13:01:56 +0100

    tweets now displayed via listTweets Page

--- client/src/components/listTweets/listTweets.js
+++ client/src/components/listTweets/listTweets.js
@@ -1,9 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListTweets = () => {
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    const response = await fetch("http://localhost:5000/tweets");
+    const jsonData = await response.json();
+
+    setTweets(jsonData);
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">List Tweets</h1>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Tweet</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tweets.map((tweet) => (
+            <tr>
+              <td>{tweet.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Fragment>
   );
 };
