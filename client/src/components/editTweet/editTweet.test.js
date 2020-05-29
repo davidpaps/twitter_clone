075a98ba9bf2f1c56ee11d0af82f6e6ea219195d
David Papamichael <davidpapamichael@gmail.com>
2020-05-29 13:40:23 +0100

    edit tweet component added, test passed

---
+++ client/src/components/editTweet/editTweet.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import EditTweet from "./editTweet";
+
+describe("Edit Tweet", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<EditTweet />)));
+
+  it("should render a <Fragment />", () => {
+    expect(wrapper.find("Fragment").length).toEqual(1);
+  });
+});
