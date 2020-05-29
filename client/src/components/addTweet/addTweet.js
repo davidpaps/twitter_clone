2d8af49b3855c2b8d5914cb503e7100a4ac95ec6
David Papamichael <davidpapamichael@gmail.com>
2020-05-29 12:48:08 +0100

    addtweets linked to databse

--- client/src/components/addTweet/addTweet.js
+++ client/src/components/addTweet/addTweet.js
@@ -1,11 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const AddTweet = () => {
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+
+    const body = { description };
+    const response = await fetch("http://localhost:5000/tweets", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    window.location = "/";
+  };
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Add a Tweet</h1>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" />
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
         <button className="btn btn-success">Add</button>
       </form>
     </Fragment>
