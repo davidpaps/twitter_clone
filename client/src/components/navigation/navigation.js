5227d9f9e4597b0c93195d4bb4390e5573b17445
David Papamichael <davidpapamichael@gmail.com>
2020-06-23 17:42:37 +0100

    navbar changed to cc to hold component did mount

--- client/src/components/navigation/navigation.js
+++ client/src/components/navigation/navigation.js
@@ -1,54 +1,70 @@
 import React, { Fragment } from "react";
 import auth from "../auth";
 
-const Navigation = () => {
-  return (
-    <Fragment>
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">
-            <img
-              src="https://vignette.wikia.nocookie.net/logopedia/images/9/99/Twitter_2012.svg/revision/latest/scale-to-width-down/340?cb=20180321032720"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt=""
-              loading="lazy"
-            />
-            Twitter
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse collapse">
-            <ul className="nav navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="/home">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-            </ul>
-          </div>
+class Navigation extends React.Component {
+  state = { valid: false };
 
-          <ul className="nav navbar-nav navbar-right">
-            <li className="nav-item active">
-              <a className="nav-link" href="/sign_out">
-                Sign Out
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </Fragment>
-  );
-};
+  componentDidMount = () => {
+    if (window.location.pathname === "/home") {
+      this.setState({ valid: true });
+    }
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">
+              <img
+                src="https://vignette.wikia.nocookie.net/logopedia/images/9/99/Twitter_2012.svg/revision/latest/scale-to-width-down/340?cb=20180321032720"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt=""
+                loading="lazy"
+              />
+              Twitter
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="navbar-collapse collapse"></div>
+
+            {this.state.valid ? (
+              <div>
+                <ul className="nav navbar-nav navbar-right">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="/sign_out">
+                      Sign Out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div>
+                <ul className="nav navbar-nav">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="/home">
+                      Home <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </nav>
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Navigation;
