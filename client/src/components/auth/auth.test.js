359dc9148fc608dd2ee5c990ee048804c1f8d833
David Papamichael <davidpapamichael@gmail.com>
2020-06-25 16:05:00 +0100

    auth test added

--- client/src/components/auth/auth.test.js
+++ client/src/components/auth/auth.test.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { shallow } from "enzyme";
 import Auth from "./auth";
 
 describe("Auth", () => {
-  let wrapper;
-
-  // beforeEach(() => (auth = new Auth()));
-
   it("", () => {
-    expect().toEqual();
+    expect(Auth.isAuthenticated()).toEqual(false);
   });
 });
