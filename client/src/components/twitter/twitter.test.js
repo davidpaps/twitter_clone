f52ebea4cd385f6f16d32dac86245b4af9434c93
David Papamichael <davidpapamichael@gmail.com>
2020-06-29 17:03:48 +0100

    Twitter tested and snapshotted

--- client/src/components/twitter/twitter.test.js
+++ client/src/components/twitter/twitter.test.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Home from "./twitter";
+import Twitter from "./twitter";
+import renderer from "react-test-renderer";
 
 describe("Home", () => {
   let wrapper;
 
-  beforeEach(() => (wrapper = shallow(<Home />)));
+  beforeEach(() => (wrapper = shallow(<Twitter />)));
 
   it("should render a <div />", () => {
     expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should render correctly", () => {
+    const tree = renderer.create(<Twitter />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
