d5f88150cf1060268eb94bf697257890414740d1
David Papamichael <davidpapamichael@gmail.com>
2020-06-30 17:33:44 +0100

    error message image added

--- client/src/components/error/error.js
+++ client/src/components/error/error.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div>
-      <h1 className="text-center mt-5">Error - Page Does Not Exist!</h1>
+    <div className="container text-center">
+      <h1 className="text-center">Error - Page Does Not Exist!</h1>
+      <img
+        src="https://www.oho.com/sites/default/files/styles/width_1160/public/2020-03/shutterstock_1076235776.jpg?itok=gNVLEOJt"
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 };
